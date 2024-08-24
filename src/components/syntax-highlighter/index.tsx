848c28f9445acd883ee5c8b487a99db9ec76afc2
kaan.cetin <kaan.cetin@trt.net.tr>
2024-08-25 00:15:02 +0300

    Syntax HL geliştirmeye devam edildi.

--- src/components/syntax-highlighter/index.tsx
+++ src/components/syntax-highlighter/index.tsx
@@ -5,10 +5,7 @@ const SyntaxHighlighter: React.FC<{ children: React.ReactNode }> = ({ children }
   // refs
   const _div = useRef<HTMLDivElement>(null);
   const _code = useRef<HTMLElement>(null);
-
-  const container = (element: string) => `<span class="ar-language">${element}</span>`;
-  // tag variable/s
-  // const lineBreakSpaces = 6;
+  let _lineBreakSpaces = useRef<number>(4).current;
 
   // states
   const [elements, setElements] = useState<string[]>([]);
@@ -16,12 +13,12 @@ const SyntaxHighlighter: React.FC<{ children: React.ReactNode }> = ({ children }
   // methods
   const handleEntries = (propValue: any): string | number => {
     if (propValue && typeof propValue === "object") {
-      return `{${Object.entries(propValue)
+      return `[curly-bracket<] ${Object.entries(propValue)
         .map(
           ([key, value]) =>
-            `[attribute-key]${key}[/attribute-key]: [object]${handleEntries(value)}[/object]`
+            `[attribute-key]${key}[/attribute-key][colon]:[/colon] ${handleEntries(value)}`
         )
-        .join(", ")}}`;
+        .join(", ")} [curly-bracket>]`;
     }
 
     return typeof propValue === "number"
@@ -33,11 +30,15 @@ const SyntaxHighlighter: React.FC<{ children: React.ReactNode }> = ({ children }
     child:
       | React.ReactPortal
       | React.ReactElement<unknown, string | React.JSXElementConstructor<any>>,
-    subChilde: boolean = false
+    subChilde: boolean = false,
+    indentLevel: number = 0
   ): string | undefined => {
     if (!child) return;
 
     const componentType = typeof child.type === "function" ? child.type.name : child.type;
+    let componentContent = child.props.children;
+    // İndent seviyesi için boşlukları ekle.
+    const indent = "  ".repeat(indentLevel);
 
     // Eğer `br` elementi ise işlemi sonlandır
     if (componentType === "br") return;
@@ -59,7 +60,9 @@ const SyntaxHighlighter: React.FC<{ children: React.ReactNode }> = ({ children }
           const entries = Object.entries(value)
             .map(
               ([subKey, subValue]) =>
-                `[attribute-key]${subKey}[/attribute-key]: ${handleEntries(subValue)}`
+                `[attribute-key]${subKey}[/attribute-key][colon]:[/colon] ${handleEntries(
+                  subValue
+                )}`
             )
             .join(", ");
 
@@ -75,8 +78,9 @@ const SyntaxHighlighter: React.FC<{ children: React.ReactNode }> = ({ children }
       return `[attribute-key]${key}[/attribute-key][equal]=[/equal]${result}`;
     };
 
-    const attributes = Object.keys(child.props)
-      .filter((key) => key !== "children")
+    const attributes = Object.keys(child.props).filter((key) => key !== "children");
+    const attributesLength = attributes.length;
+    const attributesList = attributes
       .map((key) => formatAttributeValue(key, child.props[key]))
       .join(" ");
 
@@ -84,16 +88,28 @@ const SyntaxHighlighter: React.FC<{ children: React.ReactNode }> = ({ children }
       componentType[0].toUpperCase() === componentType[0]
         ? `[react-tag]${componentType}[/react-tag]`
         : componentType;
-    const formattedAttributes = attributes ? ` [attributes]${attributes}[/attributes]` : "";
+    const formattedAttributes =
+      attributesLength > 0 ? ` [attributes]${attributesList}[/attributes]` : "";
 
-    let childrenContent = child.props.children;
-    // if (React.isValidElement(childrenContent)) {
-    //   childrenContent = `${handleComponentToString(child.props.children, true)}`;
-    // }
+    // Eğer gelen iç eleman bir nesneyse yeniden yapılan.
+    if (React.isValidElement(componentContent)) {
+      // İç içe tek bir çocuk eleman varsa recursive olarak işlemeye devam et
+      componentContent = `\n${handleComponentToString(componentContent, true, indentLevel + 1)}\n`;
+    } else if (Array.isArray(componentContent)) {
+      // Eğer birden fazla çocuk varsa hepsini işle
+      componentContent = componentContent
+        .map((contentChild) => handleComponentToString(contentChild, true))
+        .join(`\n${indent}`);
+    } else if (typeof componentContent === "string") {
+      // Eğer metin içeriği varsa, trimle
+      componentContent = componentContent.trim();
+    }
 
-    const renderElement = childrenContent
-      ? `[open]<[/open][tag]${formattedTag}[/tag]${formattedAttributes}[close]>[/close]${childrenContent}[open]<[/open][close-slash]/[/close-slash][tag]${formattedTag}[/tag][close]>[/close]`
-      : `[open]<[/open][tag]${formattedTag}[/tag]${formattedAttributes} [close]/>[/close]`;
+    componentContent =
+      attributesLength >= _lineBreakSpaces ? `\n   ${componentContent}\n` : componentContent;
+    const renderElement = componentContent
+      ? `${indent}[open]&lt;[/open][tag]${formattedTag}[/tag]${formattedAttributes}[close]>[/close]${componentContent}${indent}[open]&lt;/[/open][tag]${formattedTag}[/tag][close]>[/close]`
+      : `${indent}[open]&lt;[/open][tag]${formattedTag}[/tag]${formattedAttributes} [close]/&gt;[/close]`;
 
     !subChilde && setElements((prevElements) => [...prevElements, renderElement]);
 
@@ -152,93 +168,31 @@ const SyntaxHighlighter: React.FC<{ children: React.ReactNode }> = ({ children }
         return `<span>${p1}</span>`;
       });
 
+      element = element.replace(/\[colon\](.*?)\[\/colon\]/g, (_, p1) => {
+        return `<span>${p1}</span>`;
+      });
+
       // Attributes Values
       element = element.replace(/\[string\](.*?)\[\/string\]/g, (_, p1) => {
         return `<span class="ar-string-value">"${p1}"</span>`;
       });
+      element = element.replace(/\[number\](.*?)\[\/number\]/g, (_, p1) => {
+        return `<span class="ar-number-value">${p1}</span>`;
+      });
 
       element = element.replace(/\[boolean\](.*?)\[\/boolean\]/g, (_, p1) => {
-        return `<span class="ar-boolean-value"><span>{</span> ${p1} <span>}</span></span>`;
+        return `<span class="ar-boolean-value"><span>{</span>${p1}<span>}</span></span>`;
       });
 
       element = element.replace(/\[object\](.*?)\[\/object\]/g, (_, p1) => {
         return `<span class="ar-object-value"><span>{{</span> ${p1} <span>}}</span></span>`;
       });
 
-      if (_code.current) _code.current.innerHTML += container(element);
+      element = element.replace(/\[curly-bracket<\]/g, "<span class='ar-curly-bracket'>{</span>");
+      element = element.replace(/\[curly-bracket>\]/g, "<span class='ar-curly-bracket'>}</span>");
+
+      if (_code.current) _code.current.innerHTML += `<span class="ar-language">${element}</span>`;
     });
-
-    // elements.forEach((element) => {
-    //   element = element.replace(/<(.*?)>/g, (match, tagContent: string) => {
-    //     const equalsMatches = match.match(/=/g);
-    //     const lineBreak =
-    //       equalsMatches && equalsMatches.length >= lineBreakSpaces
-    //         ? `\n  `
-    //         : "";
-    //     // Attr için özel yazıldı.
-    //     const lineBreakAttributes =
-    //       equalsMatches && equalsMatches.length >= lineBreakSpaces ? `\n` : "";
-    //     // Bu kısım nesnelerin girintileri ve alt satıra kaydırma işlemleri için özel yazılmıştır.
-    //     const lineBreakChildStart =
-    //       equalsMatches && equalsMatches.length >= lineBreakSpaces
-    //         ? `\n    `
-    //         : "";
-    //     const lineBreakChildEnd =
-    //       equalsMatches && equalsMatches.length >= lineBreakSpaces
-    //         ? `\n  `
-    //         : "";
-
-    //     // tagContent = tagContent.replace(/\[tag\](.*?)\[\/tag\]/g, (_, p1) => {
-    //     //   return `<span class="ar-custom-tag">${p1}</span>`;
-    //     // });
-
-    //     // tagContent = tagContent.replace(/\[attributes\](.*?)\[\/attributes\]/g, (_, p1) => {
-    //     //   return `${p1}${lineBreakAttributes}`;
-    //     // });
-
-    //     // tagContent = tagContent.replace(/\[tag-slash\](.*?)\[\/tag-slash\]/g, (_, p1) => {
-    //     //   return `<span class="ar-custom-tag-slash">${p1}</span>`;
-    //     // });
-
-    //     // tagContent = tagContent.replace(/(\w+)=\s*\[number\](.*?)\[\/number\]/g, (_, p1, p2) => {
-    //     //   return `${lineBreak}${attribute(p1)}${equal}${oCurlyBracket}${entries(p2)}${cCurlyBracket}`;
-    //     // });
-
-    //     // tagContent = tagContent.replace(/([\w-]+)=\s*\[string\](.*?)\[\/string\]/g, (_, p1, p2) => {
-    //     //   return `${lineBreak}${attribute(p1)}${equal}${string(p2)}`;
-    //     // });
-
-    //     // tagContent = tagContent.replace(/(\w+)=\s*{(.*?)}/g, (_, p1, p2) => {
-    //     //   return `${lineBreak}${attribute(p1)}${equal}${oCurlyBracket}${entries(p2)}${cCurlyBracket}`;
-    //     // });
-
-    //     // tagContent = tagContent.replace(/(\w+)=\s*\[boolean\](.*?)\[\/boolean\]/g, (_, p1, p2) => {
-    //     //   return `${lineBreak}${attribute(p1)}${equal}${oCurlyBracket}${entries(p2)}${cCurlyBracket}`;
-    //     // });
-
-    //     // tagContent = tagContent.replace(/(\w+)=\s*\[object\](.*?)\[\/object\]/g, (_, p1, p2) => {
-    //     //   p2 = p2.replace(/(\w+):\s*\[child-string\](.*?)\[\/child-string\]/g, (_: string, p1: string, p2: string) => {
-    //     //     return `${lineBreakChildStart}${attribute(p1)}${colon} ${string(p2)}`;
-    //     //   });
-
-    //     //   p2 = p2.replace(/(\w+):\s*\[child-object\](.*?)\[\/child-object\]/g, (_: string, p1: string, p2: string) => {
-    //     //     return `${attribute(p1)}${colon} ${entries(p2)}`;
-    //     //   });
-
-    //     //   return `${lineBreak}${attribute(p1)}${equal}${oCurlyBrackets}${entries(p2)}${lineBreakChildEnd}${cCurlyBrackets}`;
-    //     // });
-
-    //     // tagContent = tagContent.replace(/(\w+)=\s*\[function\](.*?)\[\/function\]/g, (_, p1, p2) => {
-    //     //   return `${lineBreak}${attribute(p1)}${equal}${oCurlyBracket}()=>{${p2}}${cCurlyBracket}`;
-    //     // });
-
-    //     return !match.toLowerCase().includes("ar-")
-    //       ? `<span class="ar-tag-container"><span class="ar-tag-lt"><</span><span class="ar-tag">${tagContent}</span><span class="ar-tag-gt">></span></span>`
-    //       : match;
-    //   });
-
-    //   if (_code.current) _code.current.innerHTML += container(element);
-    // });
   }, [elements]);
 
   return (
