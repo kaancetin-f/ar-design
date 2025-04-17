bdde5c7043ad922fa8b269a74179a83c7616979e
kaan.cetin <kaan.cetin@trt.net.tr>
2025-04-17 17:22:00 +0300

    Save.

--- src/components/form/radio/IProps.ts
+++ src/components/form/radio/IProps.ts
@@ -1,11 +1,13 @@
 import React from "react";
 import { IGlobalProps, ISizes } from "../../../libs/types/IGlobalProps";
+import { Status } from "../../../libs/types";
 
 interface IProps
   extends Omit<IGlobalProps, "children">,
     ISizes,
     Omit<React.InputHTMLAttributes<HTMLInputElement>, "children" | "size"> {
   label?: string;
+  trace?: { color: Status };
 }
 
 export default IProps;
--- src/components/form/radio/index.tsx
+++ src/components/form/radio/index.tsx
@@ -6,12 +6,15 @@ import "../../../assets/css/components/form/radio/radio.css";
 import Utils from "../../../libs/infrastructure/shared/Utils";
 
 const Radio = forwardRef<HTMLInputElement, IProps>(
-  ({ label, size = "normal", status, border = { radius: "sm" }, upperCase, ...attributes }, ref) => {
+  ({ label, size = "normal", status, border = { radius: "sm" }, trace, upperCase, ...attributes }, ref) => {
     // refs
     const _checkbox = useRef<HTMLInputElement>(null);
     const _checkboxClassName: string[] = ["ar-radio"];
+    const _traceClassName: string[] = ["trace", "filled"];
 
     _checkboxClassName.push(...Utils.GetClassName("filled", status, border, size, undefined, attributes.className));
+
+    if (trace && Object.keys(trace).length > 0) _traceClassName.push(trace.color);
 
     return (
       <div className="ar-radio-wrapper">
@@ -33,6 +36,9 @@ const Radio = forwardRef<HTMLInputElement, IProps>(
 
           <span>
             <span ref={_checkbox} className={_checkboxClassName.map((c) => c).join(" ")}></span>
+            {trace && Object.keys(trace).length > 0 && (
+              <span className={_traceClassName.map((c) => c).join(" ")}></span>
+            )}
             {label && <span className="label">{upperCase ? label.toUpperCase() : label}</span>}
           </span>
         </label>
