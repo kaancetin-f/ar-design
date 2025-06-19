aa006898ab288d1dafbddda36e93c95a43f0d5c0
kaan.cetin <kaan.cetin@trt.net.tr>
2025-06-19 11:50:24 +0300

    Save.

--- src/components/feedback/popup/index.tsx
+++ src/components/feedback/popup/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import IProps from "./IProps";
 import "../../../assets/css/components/feedback/popup/popup.css";
 import Button from "../../form/button";
@@ -8,8 +8,15 @@ import ReactDOM from "react-dom";
 import { NotificationContext } from "../../../libs/core/application/contexts/Notification";
 
 const Popup = ({ title, message, status, isOpen, buttons }: IProps) => {
+  // contexts
   const { setIsPopupOpen } = useContext(NotificationContext);
+
+  // refs
+  const _arNotificationPopupWrapper = useRef<HTMLDivElement>(null);
+  const _arNotificationPopup = useRef<HTMLDivElement>(null);
+
   // states
+  const _notificaitonPopupWrapperClassName: string[] = ["ar-notification-popup-wrapper"];
   const [className, setClassName] = useState<string[]>(["ar-notification-popup", ""]);
 
   // useEffects
@@ -19,32 +26,56 @@ const Popup = ({ title, message, status, isOpen, buttons }: IProps) => {
 
       return updated;
     });
+
+    if (isOpen) document.body.style.overflow = "hidden";
+
+    return () => {
+      document.body.style.removeProperty("overflow");
+    };
   }, [isOpen]);
 
   return (
     isOpen &&
     ReactDOM.createPortal(
-      <div className={className.map((c) => c).join(" ")}>
-        <div className={`icon ${status}`}></div>
+      <div ref={_arNotificationPopupWrapper} className={_notificaitonPopupWrapperClassName.map((c) => c).join(" ")}>
+        <div
+          className="ar-notification-popup-bg"
+          onMouseDown={(event) => {
+            event.stopPropagation();
 
-        <div className="content">
-          <span className={`title ${status}`}>{title}!</span>
-          <span className="message">{message}</span>
-        </div>
+            const target = event.target as HTMLElement;
+            if (_arNotificationPopup.current && !_arNotificationPopup.current.contains(target)) {
+              setClassName((prev) => {
+                const updated = [...prev.slice(0, -1), isOpen ? "open" : ""];
 
-        <Button
-          variant="filled"
-          status={status == "success" ? "success" : "danger"}
-          onClick={() => {
-            (() => buttons?.okay?.onClick && buttons.okay?.onClick())();
-
-            (() => {
-              setIsPopupOpen && setIsPopupOpen((prev) => !prev);
-            })();
+                return updated;
+              });
+            }
           }}
-        >
-          {buttons?.okay?.text ?? "Tamam"}
-        </Button>
+        ></div>
+
+        <div ref={_arNotificationPopup} className={className.map((c) => c).join(" ")}>
+          <div className={`icon ${status}`}></div>
+
+          <div className="content">
+            <span className={`title ${status}`}>{title}!</span>
+            <span className="message">{message}</span>
+          </div>
+
+          <Button
+            variant="filled"
+            status={status == "success" ? "success" : "danger"}
+            onClick={() => {
+              (() => buttons?.okay?.onClick && buttons.okay?.onClick())();
+
+              (() => {
+                setIsPopupOpen && setIsPopupOpen((prev) => !prev);
+              })();
+            }}
+          >
+            {buttons?.okay?.text ?? "Tamam"}
+          </Button>
+        </div>
       </div>,
       document.body
     )
