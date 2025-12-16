d88d7d17937e0927a8d75e9774a8bbb851f532e0
kaan.cetin <kaan.cetin@trt.net.tr>
2025-12-16 19:12:05 +0300

    Save.

--- src/components/data-display/tabs/skeleton.tsx
+++ src/components/data-display/tabs/skeleton.tsx
@@ -8,9 +8,9 @@ const Skeleton = ({ tabs = [] }: IProps) => {
         {tabs.length > 0 &&
           tabs.map((tab, index) => {
             return (
-              <div key={tab.title ?? index} className="item" style={{ width: `${tab.title.length}px` }}>
+              <div key={index} className="item" style={{ width: `${tab.title.length}px` }}>
                 {Array.from({ length: 3 }, (_, i) => (
-                  <span style={{ animationDelay: `${i++}s` }}></span>
+                  <span key={i} style={{ animationDelay: `${i++}s` }}></span>
                 ))}
               </div>
             );
