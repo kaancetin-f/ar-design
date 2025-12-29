a0f3727418a388fca1ab86551b1170f8239ec0e5
kaan.cetin <kaan.cetin@trt.net.tr>
2025-12-29 16:01:16 +0300

    Save

--- src/components/form/switch/index.tsx
+++ src/components/form/switch/index.tsx
@@ -1,66 +1,74 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
+import React, { forwardRef, useEffect, useImperativeHandle, useRef, useState } from "react";
 import "../../../assets/css/components/form/switch/styles.css";
 import IProps from "./IProps";
 import Utils from "../../../libs/infrastructure/shared/Utils";
 
-const Switch: React.FC<IProps> = ({ label, color, border = { radius: "pill" }, ...attributes }) => {
-  // refs
-  let _switch = useRef<HTMLInputElement>(null);
-  const _inputClassName: string[] = [];
-  const _switchClassName: string[] = ["ar-switch"];
+const Switch = forwardRef(
+  ({ label, color, border = { radius: "pill" }, ...attributes }: IProps, ref: React.ForwardedRef<HTMLInputElement>) => {
+    // refs
+    let _switchInput = useRef<HTMLInputElement>(null);
+    let _switch = useRef<HTMLInputElement>(null);
+    const _inputClassName: string[] = [];
+    const _switchClassName: string[] = ["ar-switch"];
 
-  // states
-  const [checked, setChecked] = useState<boolean>(attributes.checked ?? false);
+    // states
+    const [checked, setChecked] = useState<boolean>(attributes.checked ?? false);
 
-  _inputClassName.push(attributes.checked ? "checked" : "unchecked");
-  _switchClassName.push(
-    ...Utils.GetClassName(
-      undefined,
-      undefined,
-      attributes.checked ? color : "light",
-      border,
-      undefined,
-      undefined,
-      attributes.className
-    )
-  );
+    _inputClassName.push(attributes.checked ? "checked" : "unchecked");
+    _switchClassName.push(
+      ...Utils.GetClassName(
+        undefined,
+        undefined,
+        attributes.checked ? color : "light",
+        border,
+        undefined,
+        undefined,
+        attributes.className
+      )
+    );
 
-  // useEffects
-  useEffect(() => {
-    setChecked(attributes.checked ?? false);
-  }, [attributes.checked]);
+    // hooks
+    // Dışarıdan gelen ref'i _innerRef'e bağla.
+    useImperativeHandle(ref, () => _switchInput.current as HTMLInputElement);
 
-  return (
-    <div className="ar-switch-wrapper">
-      <label>
-        <input
-          type={"checkbox"}
-          {...attributes}
-          className={_inputClassName.map((c) => c).join(" ")}
-          checked={checked}
-          size={0}
-          onChange={(event) => {
-            event.stopPropagation();
+    // useEffects
+    useEffect(() => {
+      setChecked(attributes.checked ?? false);
+    }, [attributes.checked]);
 
-            (() => {
-              setChecked(event.target.checked);
-            })();
+    return (
+      <div className="ar-switch-wrapper">
+        <label>
+          <input
+            ref={_switchInput}
+            type={"checkbox"}
+            {...attributes}
+            className={_inputClassName.map((c) => c).join(" ")}
+            checked={checked}
+            size={0}
+            onChange={(event) => {
+              event.stopPropagation();
 
-            (() => attributes.onChange && attributes.onChange(event))();
-          }}
-        />
+              (() => {
+                setChecked(event.target.checked);
+              })();
 
-        <span ref={_switch} className={_switchClassName.map((c) => c).join(" ")}>
-          <span className="handle border-radius-pill"></span>
-        </span>
+              (() => attributes.onChange && attributes.onChange(event))();
+            }}
+          />
 
-        {label && <span className="label">{label}</span>}
-      </label>
-    </div>
-  );
-};
+          <span ref={_switch} className={_switchClassName.map((c) => c).join(" ")}>
+            <span className="handle border-radius-pill"></span>
+          </span>
+
+          {label && <span className="label">{label}</span>}
+        </label>
+      </div>
+    );
+  }
+);
 
 Switch.displayName = "Switch";
 export default Switch;
