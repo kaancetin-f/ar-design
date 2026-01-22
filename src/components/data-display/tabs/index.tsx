15201bc15b08824a253fded35017ffe6e25fc9b8
kaan.cetin <kaan.cetin@trt.net.tr>
2026-01-22 18:11:51 +0300

    Save.

--- src/components/data-display/tabs/index.tsx
+++ src/components/data-display/tabs/index.tsx
@@ -1,15 +1,70 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import IProps from "./IProps";
 import "../../../assets/css/components/data-display/tabs/tabs.css";
 import Skeleton from "./skeleton";
+import { ARIcon } from "../../icons";
 
 const Tabs: React.FC<IProps> = ({ name, tabs = [], activeTab, onChange, onClose }) => {
+  // ref
+  const _container = useRef<HTMLDivElement>(null);
+  const _items = useRef<(HTMLDivElement | null)[]>([]);
+  const _itemIndex = useRef<number>(0);
+
   // states
   const [currentTab, setCurrentTab] = useState<number>(0);
+  const [scrollInfo, setScrollInfo] = useState({
+    isMaxLeft: true,
+    isMaxRight: false,
+    current: 0,
+    maxScrollable: 0,
+  });
   // states -> Loading
   const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  // methods
+  const handleScroll = () => {
+    if (_container.current) {
+      const { scrollLeft, scrollWidth, clientWidth } = _container.current;
+
+      // Maksimum sağ değer: Toplam genişlik - Görünür genişlik
+      const maxRight = scrollWidth - clientWidth;
+
+      setScrollInfo({
+        isMaxLeft: scrollLeft <= 0, // En solda mı?
+        isMaxRight: scrollLeft >= maxRight - 1, // En sağda mı?
+        current: scrollLeft, // Anlık ne kadar sağda.
+        maxScrollable: maxRight, // Gidebileceği max piksel.
+      });
+    }
+  };
+
+  const scroll = useMemo(() => {
+    return (direction: "left" | "right") => {
+      const container = _container.current;
+
+      if (!container) return;
+
+      let nextIndex = direction === "left" ? _itemIndex.current - 1 : _itemIndex.current + 1;
+
+      if (nextIndex < 0) nextIndex = 0;
+      if (nextIndex >= _items.current.length) nextIndex = _items.current.length - 1;
+
+      const targetItem = _items.current[nextIndex];
+
+      if (targetItem) {
+        const targetLeft = targetItem.offsetLeft - container.offsetLeft;
+
+        container.scrollTo({
+          left: targetLeft,
+          behavior: "smooth",
+        });
+
+        _itemIndex.current = nextIndex;
+      }
+    };
+  }, []);
 
   // useEffects
   useEffect(() => setCurrentTab(activeTab ?? 0), [activeTab]);
@@ -24,44 +79,72 @@ const Tabs: React.FC<IProps> = ({ name, tabs = [], activeTab, onChange, onClose 
     setIsLoading(false);
   }, [name]);
 
+  useEffect(() => {
+    window.addEventListener("resize", handleScroll);
+
+    return () => {
+      window.removeEventListener("resize", handleScroll);
+    };
+  }, []);
+
   if (isLoading) return <Skeleton name={name} tabs={tabs} />;
 
   return (
     <div className="ar-tabs">
       <div className="tabs">
-        {tabs.length > 0 &&
-          tabs.map((tab, index) => {
-            let className: string[] = ["item"];
+        {scrollInfo.current > 0 && (
+          <div className="left" onClick={() => scroll("left")}>
+            <ARIcon icon={"ArrowLeft"} stroke="var(--gray-700)" />
+          </div>
+        )}
 
-            if (currentTab === index) className.push("selection");
+        {!scrollInfo.isMaxRight && (
+          <div className="right" onClick={() => scroll("right")}>
+            <ARIcon icon={"ArrowRight"} stroke="var(--gray-700)" />
+          </div>
+        )}
 
-            return (
-              <div
-                key={tab.title ?? index}
-                className={className.map((c) => c).join(" ")}
-                onClick={() => {
-                  setCurrentTab(index);
-                  onChange?.(index);
+        <div ref={_container} className="container" onScroll={handleScroll}>
+          {tabs.length > 0 &&
+            tabs.map((tab, index) => {
+              let className: string[] = ["item"];
 
-                  const key = `${window.location.pathname}::${name}`;
-                  sessionStorage.setItem(key, String(index));
-                }}
-              >
-                <span>{tab.title}</span>
-                {tab.config?.canBeClosed && (
-                  <span
-                    className="close-button"
-                    onClick={(event) => {
-                      event.stopPropagation();
-                      onClose && onClose(index);
-                    }}
-                  >
-                    ✖
-                  </span>
-                )}
-              </div>
-            );
-          })}
+              if (currentTab === index) className.push("selection");
+
+              return (
+                <div
+                  key={tab.title ?? index}
+                  ref={(element) => {
+                    if (!element) return;
+
+                    _items.current[index] = element;
+                  }}
+                  className={className.map((c) => c).join(" ")}
+                  onClick={() => {
+                    setCurrentTab(index);
+                    onChange?.(index);
+
+                    const key = `${window.location.pathname}::${name}`;
+                    sessionStorage.setItem(key, String(index));
+                  }}
+                >
+                  <span>{tab.title}</span>
+
+                  {tab.config?.canBeClosed && (
+                    <span
+                      className="close-button"
+                      onClick={(event) => {
+                        event.stopPropagation();
+                        onClose && onClose(index);
+                      }}
+                    >
+                      ✖
+                    </span>
+                  )}
+                </div>
+              );
+            })}
+        </div>
       </div>
 
       <div className="content">{tabs.map((tab, index) => currentTab === index && tab.content)}</div>
--- src/components/icons/Compiler.tsx
+++ src/components/icons/Compiler.tsx
@@ -19,19 +19,25 @@ class Icon {
       case "ArrowLeft":
         return (
           <path
-            d="M15 19.92L8.48 13.4c-.77-.77-.77-2.03 0-2.8L15 4.08"
+            fillRule="evenodd"
+            d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"
             stroke={this._stroke}
             strokeWidth={this._strokeWidth}
             strokeMiterlimit="10"
             strokeLinecap="round"
             strokeLinejoin="round"
-          ></path>
+          />
         );
       case "ArrowRight":
         return (
           <path
             fillRule="evenodd"
             d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"
+            stroke={this._stroke}
+            strokeWidth={this._strokeWidth}
+            strokeMiterlimit="10"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
         );
       case "Bold":
